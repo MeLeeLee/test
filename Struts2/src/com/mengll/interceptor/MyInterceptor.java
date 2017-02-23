3dfc2e8a9a86ddbf246e702b692eb0bb7e57abf7
menglili <melelee@qq.com>
2017-02-23 16:47:59 +0800

    日常提交

---
+++ Struts2/src/com/mengll/interceptor/MyInterceptor.java
@@ -0,0 +1,21 @@
+package com.mengll.interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class MyInterceptor implements Interceptor{
+	@Override
+	public void init() {
+		System.out.println("init.................");
+	}
+	@Override
+	public String intercept(ActionInvocation arg0) throws Exception {
+		System.out.println("intercept...............");
+		return null;
+	}
+	@Override
+	public void destroy() {
+		System.out.println("destory................");
+	}
+
+}
